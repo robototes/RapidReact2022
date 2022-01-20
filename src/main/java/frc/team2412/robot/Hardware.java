983f697b269c409f6c9d5c0293a326111cf7b8f6
1198159 <jeffastedman@gmail.com>
2022-01-20 11:14:51 -0800

    yay yay yay added stuff and skeleton for most

---
+++ src/main/java/frc/team2412/robot/Hardware.java
@@ -0,0 +1,10 @@
+package frc.team2412.robot;
+
+import static frc.team2412.robot.Hardware.HardwareConstants.*;
+public class Hardware {
+    public static class HardwareConstants{
+        //TODO a ton of ports and can ids yay
+    }
+    //TODO devices galore
+
+}

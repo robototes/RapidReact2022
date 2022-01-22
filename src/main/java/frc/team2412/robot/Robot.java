3f238f79e78a51ce68735bed41ba4c2ef6c7560c
Jonah <s-kowalj@bsd405.org>
2022-01-22 11:32:44 -0800

    Updated with requested changes and refactored package naming

--- src/main/java/frc/team2412/robot/Robot.java
+++ src/main/java/frc/team2412/robot/Robot.java
@@ -19,13 +19,13 @@ public class Robot extends TimedRobot {
 
 
   public final Controls controls;
-  public final Subsystems subsystems;
+  public final SubsystemContainer subsystems;
   public final Hardware hardware;
 
   private Robot(){
       instance = this;
       hardware = new Hardware();
-      subsystems = new Subsystems(hardware);
+      subsystems = new SubsystemContainer(hardware);
       controls = new Controls(subsystems);
   }
 
--- src/main/java/frc/team2412/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/team2412/robot/subsystems/ShooterSubsystem.java
@@ -1,4 +1,4 @@
-package frc.team2412.robot.subsystems;
+package frc.team2412.robot.Subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 

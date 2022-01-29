aaf43908a85eb6862fb33a2649d7bc496658b33b
MasterCreeper41 <alex.d.minascurta@gmail.com>
2022-01-28 18:02:33 -0800

    e

--- src/main/java/frc/team2412/robot/subsystem/ShooterSubsystem.java
+++ src/main/java/frc/team2412/robot/subsystem/ShooterSubsystem.java
@@ -5,8 +5,15 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ShooterSubsystem extends SubsystemBase {
-    public ShooterSubsystem(WPI_TalonFX flywheelMotor1, WPI_TalonFX flywheelMotor2, WPI_TalonFX turretMotor,
-            WPI_TalonFX hoodMotor) {
+   private final WPI_TalonFX flywheelMotor1;
+   private final WPI_TalonFX flywheelMotor2;
+   private final WPI_TalonFX turretMotor;
+   private final WPI_TalonFX hoodMotor;
 
+    public ShooterSubsystem(WPI_TalonFX flywheelMotor1, WPI_TalonFX flywheelMotor2, WPI_TalonFX turretMotor, WPI_TalonFX hoodMotor) {
+        this.flywheelMotor1 = flywheelMotor1;
+        this.flywheelMotor2 = flywheelMotor2;
+        this.turretMotor = turretMotor;
+        this.hoodMotor = hoodMotor;  
     }
 }

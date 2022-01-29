f0b98247eaf00a3ed03bd93c693600fc22a2907a
[object Object] <object-Object@outlook.com>
2022-01-28 18:29:45 -0800

    stuff

--- src/main/java/frc/team2412/robot/subsystem/ShooterSubsystem.java
+++ src/main/java/frc/team2412/robot/subsystem/ShooterSubsystem.java
@@ -5,8 +5,20 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ShooterSubsystem extends SubsystemBase {
+    private WPI_TalonFX flywheelMotor1;
+    private WPI_TalonFX flywheelMotor2;
+    private WPI_TalonFX turretMotor;
+    private WPI_TalonFX hoodMotor;
+
     public ShooterSubsystem(WPI_TalonFX flywheelMotor1, WPI_TalonFX flywheelMotor2, WPI_TalonFX turretMotor,
             WPI_TalonFX hoodMotor) {
+        this.flywheelMotor1 = flywheelMotor1;
+        this.flywheelMotor2 = flywheelMotor2;
+        this.turretMotor = turretMotor;
+        this.hoodMotor = hoodMotor;
+    }
 
+    public void startFlywheel() {
+        flywheelMotor1.set(Double.MAX_VALUE);
     }
 }

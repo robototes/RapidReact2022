129f924149ff95becebb9fc3c00709cd662cf589
Lucy <65467897+stewluj@users.noreply.github.com>
2022-01-31 19:12:19 -0800

    Settrajectory option for debugging

--- src/main/java/frc/team2412/robot/Robot.java
+++ src/main/java/frc/team2412/robot/Robot.java
@@ -4,6 +4,16 @@
 
 package frc.team2412.robot;
 
+import com.sun.jdi.Field;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import org.frcteam2910.common.control.SimplePathBuilder;
 import org.frcteam2910.common.math.RigidTransform2;
 import org.frcteam2910.common.math.Rotation2;
@@ -12,6 +22,8 @@ import org.frcteam2910.common.robot.UpdateManager;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+
+import java.util.List;
 
 public class Robot extends TimedRobot {
     /**
@@ -37,6 +49,8 @@ public class Robot extends TimedRobot {
 
     // TODO add other override methods
 
+    public Trajectory trajectory;
+    public Field2d field;
     @Override
     public void robotInit() {
         hardware = new Hardware();
@@ -45,6 +59,23 @@ public class Robot extends TimedRobot {
         updateManager = new UpdateManager(
                 subsystems.drivebaseSubsystem);
         updateManager.startLoop(5.0e-3);
+
+        // Create the trajectory to follow in autonomous. It is best to initialize
+        // trajectories here to avoid wasting time in autonomous. This is an example trajectory, you do not need to
+        //to have it, just set trajectory to debug
+        trajectory =
+                TrajectoryGenerator.generateTrajectory(
+                        new Pose2d(0, 0, Rotation2d.fromDegrees(0)),
+                        List.of(new Translation2d(1, 1), new Translation2d(2, -1)),
+                        new Pose2d(3, 0, Rotation2d.fromDegrees(0)),
+                        new TrajectoryConfig(Units.feetToMeters(3.0), Units.feetToMeters(3.0)));
+
+        // Create and push Field2d to SmartDashboard.
+        field = new Field2d();
+        SmartDashboard.putData(field);
+
+        // Push the trajectory to Field2d.
+        //field.getObject("traj").setTrajectory(trajectory);
     }
 
     @Override

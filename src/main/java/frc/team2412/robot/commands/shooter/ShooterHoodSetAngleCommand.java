617c65191f0a47c64371f494c65a238d03767a3a
KangarooKoala <91924258+KangarooKoala@users.noreply.github.com>
2022-01-31 19:21:15 -0800

    Added some basic test commands

---
+++ src/main/java/frc/team2412/robot/commands/shooter/ShooterHoodSetAngleCommand.java
@@ -0,0 +1,33 @@
+package frc.team2412.robot.commands.shooter;
+
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.team2412.robot.subsystem.ShooterSubsystem;
+import frc.team2412.robot.subsystem.ShooterVisionSubsystem;
+
+public class ShooterHoodSetAngleCommand extends CommandBase {
+    private final ShooterSubsystem shooter;
+    private final DoubleSupplier angleSupplier;
+
+    public ShooterHoodSetAngleCommand(ShooterSubsystem shooter, double angle) {
+        this.shooter = shooter;
+        this.angleSupplier = () -> angle;
+    }
+
+    public ShooterHoodSetAngleCommand(ShooterSubsystem shooter, ShooterVisionSubsystem vision) {
+        this.shooter = shooter;
+        this.angleSupplier = null; // TODO: Interface with ShooterVisionSubsystem
+    }
+
+    @Override
+    public void execute() {
+        double angle = angleSupplier.getAsDouble();
+        shooter.hoodMotorSetAngle(angle);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return true;
+    }
+}
---
+++ src/main/java/frc/team2412/robot/commands/shooter/ShooterTurretUpdateAngleCommand.java
@@ -0,0 +1,28 @@
+package frc.team2412.robot.commands.shooter;
+
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.team2412.robot.subsystem.ShooterSubsystem;
+import frc.team2412.robot.subsystem.ShooterVisionSubsystem;
+
+public class ShooterTurretUpdateAngleCommand extends CommandBase {
+    private final ShooterSubsystem shooter;
+    private final DoubleSupplier angleSupplier;
+
+    public ShooterTurretUpdateAngleCommand(ShooterSubsystem shooter, ShooterVisionSubsystem vision) {
+        this.shooter = shooter;
+        this.angleSupplier = null; // TODO: Interface with ShooterVisionSubsystem
+    }
+
+    @Override
+    public void execute() {
+        double angle = angleSupplier.getAsDouble();
+        shooter.updateTurretAngle(angle);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}

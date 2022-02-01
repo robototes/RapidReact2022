057ea7c4ac11d6e58f1b08ec00167fea898cf00b
SumedhP <43325361+SumedhP@users.noreply.github.com>
2022-02-01 09:16:25 -0800

    Update ShooterVisionSubsystem.java

--- src/main/java/frc/team2412/robot/subsystem/ShooterVisionSubsystem.java
+++ src/main/java/frc/team2412/robot/subsystem/ShooterVisionSubsystem.java
@@ -1,18 +1,50 @@
 package frc.team2412.robot.subsystem;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import static org.photonvision.common.hardware.VisionLEDMode.kOff;
+import static org.photonvision.common.hardware.VisionLEDMode.kOn;
+
 import org.photonvision.PhotonCamera;
+import org.photonvision.targeting.PhotonTrackedTarget;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ShooterVisionSubsystem extends SubsystemBase {
 
     private PhotonCamera limelight;
 
+    private PhotonTrackedTarget target;
+
     public ShooterVisionSubsystem(PhotonCamera limelight) {
         this.limelight = limelight;
     }
 
+    @Override
+    public void periodic() {
+        if (hasTarget()) {
+            target = limelight.getLatestResult().getBestTarget();
+        }
+    }
+
+    public boolean hasTarget() {
+        return limelight.getLatestResult().hasTargets();
+    }
+
+    // x-axis
     public double getYaw() {
-        return limelight.getLatestResult().getBestTarget().getYaw();
+        return target.getYaw();
+    }
+
+    // y-axis
+    public double getPitch() {
+        return target.getPitch();
+    }
+
+    public void setLedOn() {
+        limelight.setLED(kOn);
+    }
+
+    public void setLedOff() {
+        limelight.setLED(kOff);
     }
 
 }

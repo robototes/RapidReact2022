7e20694fc0b45f7f9d8b67498ad4a181a3eb9372
b1nary <45185988+1198159@users.noreply.github.com>
2022-02-03 13:09:01 -0800

    Update Hardware.java

--- src/main/java/frc/team2412/robot/Hardware.java
+++ src/main/java/frc/team2412/robot/Hardware.java
@@ -20,6 +20,7 @@ import static frc.team2412.robot.Subsystems.SubsystemConstants.*;
 public class Hardware {
     public static class HardwareConstants {
         // drive can ids are range 1-19
+        //THE WHOLE REASON I MADE THE DATA CLASS IS SO WE WOULDNT HAVE TO DO THIS SMH MY HEAD
         public static final int DRIVETRAIN_FRONT_LEFT_DRIVE_MOTOR = 1, DRIVETRAIN_FRONT_RIGHT_DRIVE_MOTOR = 4,
                 DRIVETRAIN_BACK_LEFT_DRIVE_MOTOR = 7, DRIVETRAIN_BACK_RIGHT_DRIVE_MOTOR = 10;
         public static final int DRIVETRAIN_FRONT_LEFT_ANGLE_MOTOR = 2, DRIVETRAIN_FRONT_RIGHT_ANGLE_MOTOR = 5,

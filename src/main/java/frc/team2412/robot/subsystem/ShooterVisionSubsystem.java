25196558c09575313da80dee95b84bcc97f3f97e
SumedhP <43325361+SumedhP@users.noreply.github.com>
2022-02-02 17:51:46 -0800

    Implement Limelight  in Vision subsystem

--- src/main/java/frc/team2412/robot/subsystem/ShooterVisionSubsystem.java
+++ src/main/java/frc/team2412/robot/subsystem/ShooterVisionSubsystem.java
@@ -1,50 +1,57 @@
 package frc.team2412.robot.subsystem;
 
-import static org.photonvision.common.hardware.VisionLEDMode.kOff;
-import static org.photonvision.common.hardware.VisionLEDMode.kOn;
-
-import org.photonvision.PhotonCamera;
-import org.photonvision.targeting.PhotonTrackedTarget;
-
+import static frc.team2412.robot.subsystem.ShooterVisionSubsystem.ShooterVisionConstants.*;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ShooterVisionSubsystem extends SubsystemBase {
 
-    private PhotonCamera limelight;
+    public static class ShooterVisionConstants {
 
-    private PhotonTrackedTarget target;
+        public static double LIMELIGHT_ANGLE_OFFSET = 0;
+        public static double LIMELIGHT_HEIGHT_OFFSET = 0; 
+        public static double RIM_HEIGHT = 104; //8ft8in
+        public static double HEIGHT_TO_RIM = RIM_HEIGHT - LIMELIGHT_HEIGHT_OFFSET;
+    }
 
-    public ShooterVisionSubsystem(PhotonCamera limelight) {
-        this.limelight = limelight;
+    public NetworkTable limelight;
+
+    public ShooterVisionSubsystem() {
+       limelight = NetworkTableInstance.getDefault().getTable("limelight");
     }
 
     @Override
     public void periodic() {
-        if (hasTarget()) {
-            target = limelight.getLatestResult().getBestTarget();
-        }
+
     }
 
     public boolean hasTarget() {
-        return limelight.getLatestResult().hasTargets();
+        return limelight.getEntry("tv").getDouble(0) == 1;
     }
 
     // x-axis
     public double getYaw() {
-        return target.getYaw();
+        return limelight.getEntry("tx").getDouble(0);
+    }
+
+    // returns in inches
+    public double getDistance(){
+        double distance = HEIGHT_TO_RIM/Math.tan(LIMELIGHT_ANGLE_OFFSET + getPitch());
+        return distance;
     }
 
     // y-axis
     public double getPitch() {
-        return target.getPitch();
+        return limelight.getEntry("ty").getDouble(0);
     }
 
     public void setLedOn() {
-        limelight.setLED(kOn);
+        limelight.getEntry("ledMode").setValue("1");
     }
 
     public void setLedOff() {
-        limelight.setLED(kOff);
+        limelight.getEntry("ledMode").setValue("0");
     }
 
 }

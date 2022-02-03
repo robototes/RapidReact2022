f7537527ca5c2a489db0bad86c15a788e71b0a06
MasterCreeper41 <alex.d.minascurta@gmail.com>
2022-02-02 17:18:34 -0800

    EA Sports Update: changed the constructor in shooter turret update command.

--- src/main/java/frc/team2412/robot/commands/shooter/ShooterTurretUpdateAngleCommand.java
+++ src/main/java/frc/team2412/robot/commands/shooter/ShooterTurretUpdateAngleCommand.java
@@ -4,15 +4,14 @@ import java.util.function.DoubleSupplier;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.team2412.robot.subsystem.ShooterSubsystem;
-import frc.team2412.robot.subsystem.ShooterVisionSubsystem;
 
 public class ShooterTurretUpdateAngleCommand extends CommandBase {
     private final ShooterSubsystem shooter;
     private final DoubleSupplier angleSupplier;
 
-    public ShooterTurretUpdateAngleCommand(ShooterSubsystem shooter, ShooterVisionSubsystem vision) {
+    public ShooterTurretUpdateAngleCommand(ShooterSubsystem shooter, DoubleSupplier angleSupplier) {
         this.shooter = shooter;
-        this.angleSupplier = null; // TODO: Interface with ShooterVisionSubsystem
+        this.angleSupplier = angleSupplier;
         addRequirements(shooter);
     }
 

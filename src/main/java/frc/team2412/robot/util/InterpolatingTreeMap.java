12d7516cc8bbda0cfc9003525385be568036b177
MasterCreeper41 <alex.d.minascurta@gmail.com>
2022-02-02 17:29:45 -0800

    EA Sports Update: tree map stuff

---
+++ src/main/java/frc/team2412/robot/util/InterpolatingTreeMap.java
@@ -0,0 +1,7 @@
+package frc.team2412.robot.util;
+
+import java.util.TreeMap;
+
+public class InterpolatingTreeMap extends TreeMap {
+
+}
---
+++ src/main/java/frc/team2412/robot/util/ShooterDataDistancePoint.java
@@ -0,0 +1,11 @@
+package frc.team2412.robot.util;
+
+public class ShooterDataDistancePoint {
+    private final double distance, angle, power;
+
+    public ShooterDataDistancePoint(double distance, double angle, double power) {
+        this.distance = distance;
+        this.angle = angle;
+        this.power = power;
+    }
+}

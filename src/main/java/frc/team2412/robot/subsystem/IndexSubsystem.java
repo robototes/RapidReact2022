4cab6cba8911254b1dff87826124dfa399263ff0
jamesdooley4 <56771024+jamesdooley4@users.noreply.github.com>
2022-02-03 22:19:01 -0800

    Add crash

--- src/main/java/frc/team2412/robot/subsystem/IndexSubsystem.java
+++ src/main/java/frc/team2412/robot/subsystem/IndexSubsystem.java
@@ -6,6 +6,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class IndexSubsystem extends SubsystemBase {
     public IndexSubsystem(WPI_TalonFX indexMotor) {
-
+        Object obj = null;
+        obj.notify();
     }
 }

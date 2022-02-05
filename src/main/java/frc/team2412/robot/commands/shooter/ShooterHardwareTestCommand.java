e33b1f2be0784d003bab82d3a0c0f3ffb565ceee
KangarooKoala <91924258+KangarooKoala@users.noreply.github.com>
2022-02-05 13:56:59 -0800

    Replaced 0 in hardware test command with middle of turret range

--- src/main/java/frc/team2412/robot/commands/shooter/ShooterHardwareTestCommand.java
+++ src/main/java/frc/team2412/robot/commands/shooter/ShooterHardwareTestCommand.java
@@ -9,7 +9,8 @@ public class ShooterHardwareTestCommand extends SequentialCommandGroup {
     public ShooterHardwareTestCommand(ShooterSubsystem shooter) {
         addCommands(new ShooterTurretSetAngleCommand(shooter, ShooterConstants.MIN_TURRET_ANGLE),
                 new ShooterTurretSetAngleCommand(shooter, ShooterConstants.MAX_TURRET_ANGLE),
-                new ShooterTurretSetAngleCommand(shooter, 0),
+                new ShooterTurretSetAngleCommand(shooter,
+                        (ShooterConstants.MIN_TURRET_ANGLE + ShooterConstants.MAX_TURRET_ANGLE) / 2),
                 new ShooterHoodSetConstantAngleCommand(shooter, ShooterConstants.MAX_HOOD_ANGLE),
                 new ShooterHoodSetConstantAngleCommand(shooter, ShooterConstants.MIN_HOOD_ANGLE),
                 new ShooterFlywheelStartCommand(shooter),

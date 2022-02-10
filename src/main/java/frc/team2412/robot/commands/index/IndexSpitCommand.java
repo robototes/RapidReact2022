d02e104f9e7c9062d861e91ba262bdd59dde8175
jamietheraccoon <gabe_guitron@yahoo.com>
2022-02-09 18:47:33 -0800

    Make IndexSpitCommand.java

---
+++ src/main/java/frc/team2412/robot/commands/index/IndexSpitCommand.java
@@ -0,0 +1,27 @@
+package frc.team2412.robot.commands.index;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.team2412.robot.subsystem.IndexSubsystem;
+
+public class IndexSpitCommand extends CommandBase {
+    private final IndexSubsystem subsystem;
+
+    public IndexSpitCommand(IndexSubsystem subsystem) {
+        this.subsystem = subsystem;
+        addRequirements(subsystem);
+    }
+
+    @Override
+    public void initialize() {
+        // turn on both motors
+        subsystem.ingestMotorOut();
+        subsystem.feederMotorOut();
+    }
+
+    @Override
+    public boolean isFinished() {
+        subsystem.ingestMotorStop();
+        subsystem.feederMotorStop();
+        return false;
+    }
+}

8e28d4c7f90f0afe4987a6e166f1c59a0d057e5f
1toldyou <86680163+1toldyou@users.noreply.github.com>
2022-02-12 09:44:41 -0800

    more Copilot fun

--- src/main/java/frc/team2412/robot/commands/diagnostic/DiagnosticRobotCommandGroup.java
+++ src/main/java/frc/team2412/robot/commands/diagnostic/DiagnosticRobotCommandGroup.java
@@ -1,5 +1,9 @@
 package frc.team2412.robot.commands.diagnostic;
 
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.team2412.robot.subsystem.ClimbSubsystem;
 import frc.team2412.robot.subsystem.IndexSubsystem;
@@ -7,15 +11,35 @@ import frc.team2412.robot.subsystem.IntakeSubsystem;
 import frc.team2412.robot.subsystem.ShooterSubsystem;
 
 public class DiagnosticRobotCommandGroup extends SequentialCommandGroup {
+    ShuffleboardTab tab = Shuffleboard.getTab("Self Diagnostic");
+    NetworkTableEntry intakeStatus, shooterStatus, indexStatus, climbStatus;
+
     public DiagnosticRobotCommandGroup(
             IntakeSubsystem intakeSubsystem,
             ShooterSubsystem shooterSubsystem,
             IndexSubsystem indexSubsystem,
             ClimbSubsystem climbSubsystem) {
-        super(
+        /*
+         * The Shuffleboard having issue of 
+         * cannot properly display more than 3 rows of entry in one column
+         */
+        indexStatus = tab.add("Index Status", "Waiting").withPosition(0, 0).withSize(1, 1).getEntry();
+        shooterStatus = tab.add("Shooter Status", "Waiting").withPosition(1, 0).withSize(1, 1).getEntry();
+        intakeStatus = tab.add("Intake Status", "Waiting").withPosition(2, 0).withSize(1, 1).getEntry();
+        climbStatus = tab.add("Climb Status", "Waiting").withPosition(3, 0).withSize(1, 1).getEntry();
+        addRequirements(intakeSubsystem, shooterSubsystem, indexSubsystem, climbSubsystem);
+        addCommands(
+                new InstantCommand(() -> intakeStatus.setString("In Progress")),
                 new DiagnosticIntakeCommandGroup(intakeSubsystem),
+                new InstantCommand(() -> intakeStatus.setString("Finished")),
+                new InstantCommand(() -> shooterStatus.setString("In Progress")),
                 new DiagnosticShooterCommandGroup(shooterSubsystem),
+                new InstantCommand(() -> shooterStatus.setString("Finished")),
+                new InstantCommand(() -> indexStatus.setString("In Progress")),
                 new DiagnosticIndexCommandGroup(indexSubsystem),
-                new DiagnosticClimbCommandGroup(climbSubsystem));
+                new InstantCommand(() -> indexStatus.setString("Finished")),
+                new InstantCommand(() -> climbStatus.setString("In Progress")),
+                new DiagnosticClimbCommandGroup(climbSubsystem),
+                new InstantCommand(() -> climbStatus.setString("Finished")));
     }
 }

9f4f1d0d941aa07f177283101f4c69df5005ad02
1toldyou <86680163+1toldyou@users.noreply.github.com>
2022-02-12 09:29:18 -0800

    use existing test command of IndexSubsystem

--- src/main/java/frc/team2412/robot/commands/diagnostic/DiagnosticIndexCommandGroup.java
+++ src/main/java/frc/team2412/robot/commands/diagnostic/DiagnosticIndexCommandGroup.java
@@ -1,11 +1,13 @@
 package frc.team2412.robot.commands.diagnostic;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.team2412.robot.commands.index.IndexTestCommand;
 import frc.team2412.robot.subsystem.IndexSubsystem;
 
 public class DiagnosticIndexCommandGroup extends SequentialCommandGroup {
     public DiagnosticIndexCommandGroup(IndexSubsystem indexSubsystem) {
-        // TODO: wait for commands of IndexSubsystem being make
-        super();
+        super(
+                new IndexTestCommand(indexSubsystem), new WaitCommand(5));
     }
 }

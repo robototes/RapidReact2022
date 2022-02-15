608d686230c4af63209c3efd5bad11584e9dc72d
kirbt <91921906+kirbt@users.noreply.github.com>
2022-02-14 18:28:43 -0800

    optimized intake test command
    yippee wahoo

--- src/main/java/frc/team2412/robot/commands/intake/IntakeTestCommand.java
+++ src/main/java/frc/team2412/robot/commands/intake/IntakeTestCommand.java
@@ -2,6 +2,7 @@ package frc.team2412.robot.commands.intake;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.team2412.robot.commands.diagnostic.DiagnosticIntakeCommandGroup;
 import frc.team2412.robot.subsystem.IntakeSubsystem;
 
 public class IntakeTestCommand extends SequentialCommandGroup {
@@ -12,11 +13,7 @@ public class IntakeTestCommand extends SequentialCommandGroup {
         this.subsystem = subsystem;
         addCommands(
                 // first test
-                new IntakeExtendCommand(subsystem), new WaitCommand(0.5),
-                new IntakeInCommand(subsystem), new WaitCommand(0.5),
-                new IntakeOutCommand(subsystem), new WaitCommand(0.5),
-                new IntakeStopCommand(subsystem), new WaitCommand(0.5),
-                new IntakeRetractCommand(subsystem), new WaitCommand(2),
+                new DiagnosticIntakeCommandGroup(subsystem), new WaitCommand(2),
 
                 // test if motors run despite solenoid not being extended
                 new IntakeInCommand(subsystem), new WaitCommand(0.5),

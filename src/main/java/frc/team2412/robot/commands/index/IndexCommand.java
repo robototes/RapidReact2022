91df7f4337f873f5dc63eca4eb3fda7ff333db3e
kirbt <91921906+kirbt@users.noreply.github.com>
2022-02-18 18:49:16 -0800

    index command changes
    switched from feeder motor o ingest motor

--- src/main/java/frc/team2412/robot/commands/index/IndexCommand.java
+++ src/main/java/frc/team2412/robot/commands/index/IndexCommand.java
@@ -14,14 +14,14 @@ public class IndexCommand extends CommandBase {
     @Override
     public void execute() {
         if (!subsystem.feederSensorHasBallIn() || !subsystem.ingestSensorHasBallIn()) {
-            subsystem.feederMotorIn();
+            subsystem.ingestMotorIn();
         } else {
-            subsystem.feederMotorStop();
+            subsystem.ingestMotorStop();
         }
     }
 
     @Override
     public void end(boolean cancel) {
-        subsystem.feederMotorStop();
+        subsystem.ingestMotorStop();
     }
 }

2ff8c44c0f2d03a1d5adfa523a43cbda0d356607
kirbt <91921906+kirbt@users.noreply.github.com>
2022-02-18 19:04:43 -0800

    trewww!!
    shooter vision enabled is true now not false very real

--- src/main/java/frc/team2412/robot/Subsystems.java
+++ src/main/java/frc/team2412/robot/Subsystems.java
@@ -11,7 +11,7 @@ public class Subsystems implements Loggable {
         public static final boolean CLIMB_ENABLED = true;
         public static final boolean DRIVE_ENABLED = true;
         public static final boolean DRIVER_VIS_ENABLED = false;
-        public static final boolean SHOOTER_VISION_ENABLED = false;
+        public static final boolean SHOOTER_VISION_ENABLED = true;
         public static final boolean INDEX_ENABLED = true;
         public static final boolean INTAKE_ENABLED = true;
         public static final boolean SHOOTER_ENABLED = true;

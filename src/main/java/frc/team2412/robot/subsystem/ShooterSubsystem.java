71fc7eadeb1ea3ed02bf4a672a081f618744ed51
SumedhP <43325361+SumedhP@users.noreply.github.com>
2022-02-22 13:16:15 -0800

    Added manual shooter commands

--- src/main/java/frc/team2412/robot/subsystem/ShooterSubsystem.java
+++ src/main/java/frc/team2412/robot/subsystem/ShooterSubsystem.java
@@ -369,4 +369,5 @@ public class ShooterSubsystem extends SubsystemBase implements Loggable {
     public void updateTurretAngle(double deltaAngle) {
         setTurretAngle(getTurretAngle() + deltaAngle);
     }
+
 }

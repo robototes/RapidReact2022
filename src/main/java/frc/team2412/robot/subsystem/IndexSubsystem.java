c8e3ddee879512efc5b02b3f7fb0dc84294f90ff
kirbt <91921906+kirbt@users.noreply.github.com>
2022-02-22 13:18:34 -0800

    removed things from index
    i done goof

--- src/main/java/frc/team2412/robot/subsystem/IndexSubsystem.java
+++ src/main/java/frc/team2412/robot/subsystem/IndexSubsystem.java
@@ -223,14 +223,6 @@ public class IndexSubsystem extends SubsystemBase {
                 || teamColor == Alliance.Red && feederRedColor.get());
     }
 
-    public boolean ingestHasBall() {
-        return (ingestSensorHasBallIn() || ingestBlueColor.get() || ingestRedColor.get());
-    }
-
-    public boolean feederHasBall() {
-        return (ingestSensorHasBallIn() || ingestBlueColor.get() || ingestRedColor.get());
-    }
-
     // do need now! :D D: :3 8) B) :P C: xD :p :] E: :} :> .U.
     @Override
     public void periodic() {

2326b7a7472fb4bb11a40e51a762c26e92884cef
KangarooKoala <91924258+KangarooKoala@users.noreply.github.com>
2022-02-24 17:15:52 -0800

    Make spotless happy

--- src/main/java/frc/team2412/robot/commands/shooter/FullShootCommand.java
+++ src/main/java/frc/team2412/robot/commands/shooter/FullShootCommand.java
@@ -8,7 +8,8 @@ import frc.team2412.robot.subsystem.ShooterSubsystem;
 import frc.team2412.robot.subsystem.ShooterVisionSubsystem;
 
 public class FullShootCommand extends ParallelCommandGroup {
-    public FullShootCommand(ShooterSubsystem shooter, ShooterVisionSubsystem vision, IntakeSubsystem intake, IndexSubsystem index) {
+    public FullShootCommand(ShooterSubsystem shooter, ShooterVisionSubsystem vision, IntakeSubsystem intake,
+            IndexSubsystem index) {
         addCommands(new ShooterTargetCommand(shooter, vision), new IntakeInCommand(index, intake));
     }
 }

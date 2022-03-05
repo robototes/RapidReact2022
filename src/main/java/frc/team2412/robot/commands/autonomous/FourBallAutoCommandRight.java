f61a3112b6a73cfce56a1f2d1326805d2d06a1d5
DriverStation2 <None>
2022-03-04 21:27:54 -0800

    formatted

--- src/main/java/frc/team2412/robot/commands/autonomous/FourBallAutoCommandRight.java
+++ src/main/java/frc/team2412/robot/commands/autonomous/FourBallAutoCommandRight.java
@@ -22,37 +22,41 @@ import frc.team2412.robot.subsystem.ShooterVisionSubsystem;
 
 public class FourBallAutoCommandRight extends SequentialCommandGroup {
     public FourBallAutoCommandRight(IndexSubsystem indexSubsystem, ShooterSubsystem shooterSubsystem,
-        ShooterVisionSubsystem shooterVisionSubsystem, DrivebaseSubsystem drivebaseSubsystem,
-        IntakeSubsystem intakeSubsystem) {
+            ShooterVisionSubsystem shooterVisionSubsystem, DrivebaseSubsystem drivebaseSubsystem,
+            IntakeSubsystem intakeSubsystem) {
         // robot should be in the same place as two ball auto right
         // TODO: add rotation to this
         Trajectory driveBack = new Trajectory(
-            new SimplePathBuilder(new Vector2(334, 220), Rotation2.fromDegrees(68.2))
-                .lineTo(new Vector2(353, 313), Rotation2.fromDegrees(68.2))
-                .build(), 
-            DrivebaseSubsystem.DriveConstants.TRAJECTORY_CONSTRAINTS, 0.1);
-        
+                new SimplePathBuilder(new Vector2(334, 220), Rotation2.fromDegrees(68.2))
+                        .lineTo(new Vector2(353, 313), Rotation2.fromDegrees(68.2))
+                        .build(),
+                DrivebaseSubsystem.DriveConstants.TRAJECTORY_CONSTRAINTS, 0.1);
+
         Trajectory driveToTerminal = new Trajectory(
-            new SimplePathBuilder(new Vector2(353, 313), Rotation2.fromDegrees(68.2))
-                .lineTo(new Vector2(610, 280), Rotation2.fromDegrees(0))
-                .build(), 
-            DrivebaseSubsystem.DriveConstants.TRAJECTORY_CONSTRAINTS, 0.1);
+                new SimplePathBuilder(new Vector2(353, 313), Rotation2.fromDegrees(68.2))
+                        .lineTo(new Vector2(610, 280), Rotation2.fromDegrees(0))
+                        .build(),
+                DrivebaseSubsystem.DriveConstants.TRAJECTORY_CONSTRAINTS, 0.1);
 
         Trajectory driveToHub = new Trajectory(
-            new SimplePathBuilder(new Vector2(610, 280), Rotation2.fromDegrees(0))
-                .lineTo(new Vector2(452, 250), Rotation2.fromDegrees(0))
-                .build(), 
-            DrivebaseSubsystem.DriveConstants.TRAJECTORY_CONSTRAINTS, 0.1);
+                new SimplePathBuilder(new Vector2(610, 280), Rotation2.fromDegrees(0))
+                        .lineTo(new Vector2(452, 250), Rotation2.fromDegrees(0))
+                        .build(),
+                DrivebaseSubsystem.DriveConstants.TRAJECTORY_CONSTRAINTS, 0.1);
 
         addCommands(
-            new ParallelCommandGroup(
-                    new ScheduleCommand(new ShooterTargetCommand(shooterSubsystem, shooterVisionSubsystem)),
-                    new WaitCommand(1)),
-            new ParallelDeadlineGroup(new WaitCommand(1), new IndexShootCommand(indexSubsystem)),
-            new IntakeExtendCommand(intakeSubsystem),
-            new ParallelDeadlineGroup(new WaitCommand(3), new Follow2910TrajectoryCommand(drivebaseSubsystem, driveBack), new IntakeInCommand(indexSubsystem, intakeSubsystem)),
-            new ParallelDeadlineGroup(new WaitCommand(4), new Follow2910TrajectoryCommand(drivebaseSubsystem, driveToTerminal), new IntakeInCommand(indexSubsystem, intakeSubsystem)),
-            new ParallelCommandGroup(new Follow2910TrajectoryCommand(drivebaseSubsystem, driveToHub), new IntakeInCommand(indexSubsystem, intakeSubsystem))
-        );
+                new ParallelCommandGroup(
+                        new ScheduleCommand(new ShooterTargetCommand(shooterSubsystem, shooterVisionSubsystem)),
+                        new WaitCommand(1)),
+                new ParallelDeadlineGroup(new WaitCommand(1), new IndexShootCommand(indexSubsystem)),
+                new IntakeExtendCommand(intakeSubsystem),
+                new ParallelDeadlineGroup(new WaitCommand(3),
+                        new Follow2910TrajectoryCommand(drivebaseSubsystem, driveBack),
+                        new IntakeInCommand(indexSubsystem, intakeSubsystem)),
+                new ParallelDeadlineGroup(new WaitCommand(4),
+                        new Follow2910TrajectoryCommand(drivebaseSubsystem, driveToTerminal),
+                        new IntakeInCommand(indexSubsystem, intakeSubsystem)),
+                new ParallelCommandGroup(new Follow2910TrajectoryCommand(drivebaseSubsystem, driveToHub),
+                        new IntakeInCommand(indexSubsystem, intakeSubsystem)));
     }
 }

e45b61c5e38dbcdb18058622cdfa7a3baa61a792
kirbt <91921906+kirbt@users.noreply.github.com>
2022-03-08 09:48:55 -0800

    i done goof
    heheheha

---
+++ src/main/java/frc/team2412/robot/commands/intake/IntakeTestCommand.java
@@ -0,0 +1,20 @@
+package frc.team2412.robot.commands.intake;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.team2412.robot.commands.diagnostic.DiagnosticIntakeCommandGroup;
+import frc.team2412.robot.subsystem.IntakeSubsystem;
+
+public class IntakeTestCommand extends SequentialCommandGroup {
+
+    public IntakeTestCommand(IntakeSubsystem subsystem) {
+        addCommands(
+                // first test
+                new DiagnosticIntakeCommandGroup(subsystem), new WaitCommand(2),
+
+                // test if motors run despite solenoid not being extended
+                new IntakeSetInCommand(subsystem), new WaitCommand(0.5),
+                new IntakeSetOutCommand(subsystem), new WaitCommand(0.5),
+                new IntakeSetStopCommand(subsystem), new WaitCommand(0.5));
+    }
+}

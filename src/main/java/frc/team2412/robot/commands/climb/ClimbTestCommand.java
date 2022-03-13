1779d6d087a88be77ef264bc802dfb07c886d5b4
SumedhP <usa1998dec@gmail.com>
2022-03-12 18:07:55 -0800

    Fixed naimg and added waits in climbTest

--- src/main/java/frc/team2412/robot/commands/climb/ClimbTestCommand.java
+++ src/main/java/frc/team2412/robot/commands/climb/ClimbTestCommand.java
@@ -1,13 +1,15 @@
 package frc.team2412.robot.commands.climb;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.team2412.robot.subsystem.ClimbSubsystem;
 
 public class ClimbTestCommand extends SequentialCommandGroup {
 
     public ClimbTestCommand(ClimbSubsystem subsystem) {
-        addCommands(new ExtendArmCommand(subsystem),
-                new RetractArmCommand(subsystem));
+        addCommands(new ExtendArmCommand(subsystem), new WaitCommand(0.5),
+                new RetractArmCommand(subsystem), new WaitCommand(0.5),
+                new RetractArmFullyCommand(subsystem));
     }
 
 }

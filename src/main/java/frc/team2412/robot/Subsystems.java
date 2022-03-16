7c486f0998eee9896b963b913b657d0595ab8576
SumedhP <43325361+SumedhP@users.noreply.github.com>
2022-03-15 21:06:35 -0700

    Enabling subsystems
    Enabling all the subsystems again

--- src/main/java/frc/team2412/robot/Subsystems.java
+++ src/main/java/frc/team2412/robot/Subsystems.java
@@ -16,8 +16,8 @@ import io.github.oblarg.oblog.Loggable;
 
 public class Subsystems implements Loggable {
     public static class SubsystemConstants {
-        public static final boolean CLIMB_ENABLED = false;
-        public static final boolean DRIVE_ENABLED = false;
+        public static final boolean CLIMB_ENABLED = true;
+        public static final boolean DRIVE_ENABLED = true;
         public static final boolean DRIVER_VIS_ENABLED = false;
         public static final boolean SHOOTER_VISION_ENABLED = true;
         public static final boolean INDEX_ENABLED = true;

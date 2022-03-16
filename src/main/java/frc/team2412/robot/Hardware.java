bf4ca5255881808a4a0befce9335c56f95b33a07
SumedhP <43325361+SumedhP@users.noreply.github.com>
2022-03-16 12:35:56 -0700

    Merge failing

--- src/main/java/frc/team2412/robot/Hardware.java
+++ src/main/java/frc/team2412/robot/Hardware.java
@@ -1,176 +1,89 @@
 package frc.team2412.robot;
 
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel;
 import com.swervedrivespecialties.swervelib.Mk4SwerveModuleHelper;
-import com.swervedrivespecialties.swervelib.SwerveModule;
 
-import edu.wpi.first.cameraserver.CameraServer;
-import edu.wpi.first.cscore.UsbCamera;
-import edu.wpi.first.wpilibj.*;
-import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
-
-import org.frcteam2910.common.drivers.Gyroscope;
-
+import frc.team2412.robot.sim.TalonFXSimProfile.TalonFXConstants;
 import frc.team2412.robot.util.Mk4Configuration;
-import org.frcteam2910.common.robot.drivers.NavX;
-import org.frcteam2910.common.robot.drivers.Pigeon;
-
-import static frc.team2412.robot.Hardware.HardwareConstants.*;
-import static frc.team2412.robot.Subsystems.SubsystemConstants.*;
 
 public class Hardware {
-    public static class HardwareConstants {
+    // drive can ids are range 1-19 (1 is taken by power distribution module)
+    public static final int DRIVETRAIN_FRONT_LEFT_DRIVE_MOTOR = 1, DRIVETRAIN_FRONT_RIGHT_DRIVE_MOTOR = 4,
+            DRIVETRAIN_BACK_LEFT_DRIVE_MOTOR = 7, DRIVETRAIN_BACK_RIGHT_DRIVE_MOTOR = 10;
+    public static final int DRIVETRAIN_FRONT_LEFT_ANGLE_MOTOR = 2, DRIVETRAIN_FRONT_RIGHT_ANGLE_MOTOR = 5,
+            DRIVETRAIN_BACK_LEFT_ANGLE_MOTOR = 8, DRIVETRAIN_BACK_RIGHT_ANGLE_MOTOR = 11;
+    public static final int DRIVETRAIN_FRONT_LEFT_ENCODER_PORT = 3, DRIVETRAIN_FRONT_RIGHT_ENCODER_PORT = 6,
+            DRIVETRAIN_BACK_LEFT_ENCODER_PORT = 9, DRIVETRAIN_BACK_RIGHT_ENCODER_PORT = 12;
+    public static final double DRIVETRAIN_FRONT_LEFT_ENCODER_OFFSET = -Math.toRadians(67.852);
+    public static final double DRIVETRAIN_FRONT_RIGHT_ENCODER_OFFSET = -Math.toRadians(221.924);
+    public static final double DRIVETRAIN_BACK_LEFT_ENCODER_OFFSET = -Math.toRadians(214.980);
+    public static final double DRIVETRAIN_BACK_RIGHT_ENCODER_OFFSET = -Math.toRadians(168.398);
 
-        // drive can ids are range 1-19 (1 is taken by power distribution module)
-        public static final int DRIVETRAIN_FRONT_LEFT_DRIVE_MOTOR = 1, DRIVETRAIN_FRONT_RIGHT_DRIVE_MOTOR = 4,
-                DRIVETRAIN_BACK_LEFT_DRIVE_MOTOR = 7, DRIVETRAIN_BACK_RIGHT_DRIVE_MOTOR = 10;
-        public static final int DRIVETRAIN_FRONT_LEFT_ANGLE_MOTOR = 2, DRIVETRAIN_FRONT_RIGHT_ANGLE_MOTOR = 5,
-                DRIVETRAIN_BACK_LEFT_ANGLE_MOTOR = 8, DRIVETRAIN_BACK_RIGHT_ANGLE_MOTOR = 11;
-        public static final int DRIVETRAIN_FRONT_LEFT_ENCODER_PORT = 3, DRIVETRAIN_FRONT_RIGHT_ENCODER_PORT = 6,
-                DRIVETRAIN_BACK_LEFT_ENCODER_PORT = 9, DRIVETRAIN_BACK_RIGHT_ENCODER_PORT = 12;
-        public static final double DRIVETRAIN_FRONT_LEFT_ENCODER_OFFSET = -Math.toRadians(67.852);
-        public static final double DRIVETRAIN_FRONT_RIGHT_ENCODER_OFFSET = -Math.toRadians(221.924);
-        public static final double DRIVETRAIN_BACK_LEFT_ENCODER_OFFSET = -Math.toRadians(214.980);
-        public static final double DRIVETRAIN_BACK_RIGHT_ENCODER_OFFSET = -Math.toRadians(168.398);
+    // Changes swerve modules & disables subsystems missing from the swerve test bot
+    private static final Mk4SwerveModuleHelper.GearRatio GEAR_RATIO;
 
-        // Changes swerve modules & disables subsystems missing from the swerve test bot
-        private static final Mk4SwerveModuleHelper.GearRatio GEAR_RATIO;
-
-        static {
-            GEAR_RATIO = Robot.getInstance().isCompetition()
-                    ? Mk4SwerveModuleHelper.GearRatio.L2
-                    : Mk4SwerveModuleHelper.GearRatio.L1;
-        }
-
-        public static final Mk4Configuration FRONT_LEFT_CONFIG = new Mk4Configuration(
-                GEAR_RATIO,
-                DRIVETRAIN_FRONT_LEFT_DRIVE_MOTOR,
-                DRIVETRAIN_FRONT_LEFT_ANGLE_MOTOR,
-                DRIVETRAIN_FRONT_LEFT_ENCODER_PORT,
-                DRIVETRAIN_FRONT_LEFT_ENCODER_OFFSET);
-        public static final Mk4Configuration FRONT_RIGHT_CONFIG = new Mk4Configuration(
-                GEAR_RATIO,
-                DRIVETRAIN_FRONT_RIGHT_DRIVE_MOTOR,
-                DRIVETRAIN_FRONT_RIGHT_ANGLE_MOTOR,
-                DRIVETRAIN_FRONT_RIGHT_ENCODER_PORT,
-                DRIVETRAIN_FRONT_RIGHT_ENCODER_OFFSET);
-        public static final Mk4Configuration BACK_LEFT_CONFIG = new Mk4Configuration(
-                GEAR_RATIO,
-                DRIVETRAIN_BACK_LEFT_DRIVE_MOTOR,
-                DRIVETRAIN_BACK_LEFT_ANGLE_MOTOR,
-                DRIVETRAIN_BACK_LEFT_ENCODER_PORT,
-                DRIVETRAIN_BACK_LEFT_ENCODER_OFFSET);
-        public static final Mk4Configuration BACK_RIGHT_CONFIG = new Mk4Configuration(
-                GEAR_RATIO,
-                DRIVETRAIN_BACK_RIGHT_DRIVE_MOTOR,
-                DRIVETRAIN_BACK_RIGHT_ANGLE_MOTOR,
-                DRIVETRAIN_BACK_RIGHT_ENCODER_PORT,
-                DRIVETRAIN_BACK_RIGHT_ENCODER_OFFSET);
-
-        public static final double MODULE_MAX_RPM = 6000.0;
-        public static final double MODULE_MAX_VELOCITY_METERS_PER_SEC = FRONT_LEFT_CONFIG.getRatio()
-                .getConfiguration()
-                .getWheelDiameter() * Math.PI *
-                FRONT_LEFT_CONFIG.getRatio().getConfiguration().getDriveReduction() * MODULE_MAX_RPM
-                / 60.0;
-
-        public static final int GYRO_PORT = 62;
-
-        // cameras
-        public static final String LIMELIGHT = "limelight", FRONT_CAM = "front";
-
-        // shooter can ids are range 20-29
-        public static final int FLYWHEEL_1 = 20, FLYWHEEL_2 = 21, TURRET = 22, HOOD = 23;
-
-        // intake can ids are range 30-39
-        public static final int INTAKE_MOTOR = 30, INTAKE_MOTOR_2 = 31, INTAKE_SOLENOID_UP = 1,
-                INTAKE_SOLENOID_DOWN = 0, INTAKE_PROXIMITY = 8;
-
-        // index can ids are range 40-49
-        public static final int INDEX_INGEST_MOTOR = 40, INDEX_FEEDER_MOTOR = 41,
-                INDEX_PROXIMITY = 9;
-
-        // climb can ids are range 50-59
-        public static final int CLIMB_DYNAMIC_MOTOR = 50, CLIMB_FIXED_MOTOR = 51, CLIMB_ANGLE_UP_SOLENOID = 7,
-                CLIMB_ANGLE_DOWN_SOLENOID = 8, CLIMB_LIMIT_SWTICH = 30;// to be determined
-
-        // PDP logging
-        public static final int PDP_ID = 1; // needs to be verified on the bot (Can be found in REV)
-
+    static {
+        GEAR_RATIO = Robot.getInstance().isCompetition()
+                ? Mk4SwerveModuleHelper.GearRatio.L2
+                : Mk4SwerveModuleHelper.GearRatio.L1;
     }
 
-    // PDP
-    public PowerDistribution PDP;
+    public static final Mk4Configuration FRONT_LEFT_CONFIG = new Mk4Configuration(
+            GEAR_RATIO,
+            DRIVETRAIN_FRONT_LEFT_DRIVE_MOTOR,
+            DRIVETRAIN_FRONT_LEFT_ANGLE_MOTOR,
+            DRIVETRAIN_FRONT_LEFT_ENCODER_PORT,
+            DRIVETRAIN_FRONT_LEFT_ENCODER_OFFSET);
+    public static final Mk4Configuration FRONT_RIGHT_CONFIG = new Mk4Configuration(
+            GEAR_RATIO,
+            DRIVETRAIN_FRONT_RIGHT_DRIVE_MOTOR,
+            DRIVETRAIN_FRONT_RIGHT_ANGLE_MOTOR,
+            DRIVETRAIN_FRONT_RIGHT_ENCODER_PORT,
+            DRIVETRAIN_FRONT_RIGHT_ENCODER_OFFSET);
+    public static final Mk4Configuration BACK_LEFT_CONFIG = new Mk4Configuration(
+            GEAR_RATIO,
+            DRIVETRAIN_BACK_LEFT_DRIVE_MOTOR,
+            DRIVETRAIN_BACK_LEFT_ANGLE_MOTOR,
+            DRIVETRAIN_BACK_LEFT_ENCODER_PORT,
+            DRIVETRAIN_BACK_LEFT_ENCODER_OFFSET);
+    public static final Mk4Configuration BACK_RIGHT_CONFIG = new Mk4Configuration(
+            GEAR_RATIO,
+            DRIVETRAIN_BACK_RIGHT_DRIVE_MOTOR,
+            DRIVETRAIN_BACK_RIGHT_ANGLE_MOTOR,
+            DRIVETRAIN_BACK_RIGHT_ENCODER_PORT,
+            DRIVETRAIN_BACK_RIGHT_ENCODER_OFFSET);
 
-    // drive
-    public SwerveModule frontLeftModule, frontRightModule, backLeftModule, backRightModule;
-    public Gyroscope gyro;
+    public static final double MODULE_MAX_RPM = 6000.0;
+    public static final double MODULE_MAX_VELOCITY_METERS_PER_SEC = FRONT_LEFT_CONFIG.getRatio()
+            .getConfiguration()
+            .getWheelDiameter() * Math.PI *
+            FRONT_LEFT_CONFIG.getRatio().getConfiguration().getDriveReduction() * MODULE_MAX_RPM
+            / 60.0;
+
+    public static final int GYRO_PORT = 62;
 
     // cameras
-    public UsbCamera frontCamera;
+    public static final String LIMELIGHT = "limelight", FRONT_CAM = "front";
 
-    // shooter
-    public WPI_TalonFX flywheelMotor1, flywheelMotor2, turretMotor;
-    public CANSparkMax hoodMotor;
+    // shooter can ids are range 20-29
+    public static final int FLYWHEEL_1 = 20, FLYWHEEL_2 = 21, TURRET = 22, HOOD = 23;
 
-    // intake
-    public WPI_TalonFX intakeMotor, intakeMotor2;
-    public DoubleSolenoid intakeSolenoid;
-    public DigitalInput intakeProximity;
+    // intake can ids are range 30-39
+    public static final int INTAKE_MOTOR_1 = 30, INTAKE_MOTOR_2 = 31, INTAKE_SOLENOID_UP = 1,
+            INTAKE_SOLENOID_DOWN = 0;
 
-    // climb
-    public WPI_TalonFX climbMotorFixed, climbMotorDynamic;
+    // index can ids are range 40-49
+    public static final int INDEX_INGEST_MOTOR = 40, INDEX_FEEDER_MOTOR = 41, INGEST_PROXIMITY = 2,
+            FEEDER_PROXIMITY = 5;
 
-    public DoubleSolenoid climbAngle;
+    // climb can ids are range 50-59
+    public static final int CLIMB_FIXED_MOTOR = 51;
+    public static final int CLIMB_LIMIT_SWITCH = 9; // to be determined - digital I/O pins are 0-9
 
-    public DigitalInput climbLimitSwitch;
+    // Other hardware
+    public static final int PDP_ID = 1; // needs to be verified on the bot (Can be found in REV)
+    public static final int PNEUMATIC_HUB = 60;
 
-    // index
-    public WPI_TalonFX ingestIndexMotor, feederIndexMotor;
-    public DigitalInput indexProximity;
-
-    public Hardware() {
-        boolean comp = Robot.getInstance().isCompetition();
-
-        PDP = new PowerDistribution(PDP_ID, ModuleType.kRev);
-
-        if (DRIVE_ENABLED) {
-            frontLeftModule = FRONT_LEFT_CONFIG.create(comp && !Robot.isSimulation());
-            frontRightModule = FRONT_RIGHT_CONFIG.create(comp && !Robot.isSimulation());
-            backLeftModule = BACK_LEFT_CONFIG.create(comp && !Robot.isSimulation());
-            backRightModule = BACK_RIGHT_CONFIG.create(comp && !Robot.isSimulation());
-            gyro = comp ? new Pigeon(GYRO_PORT) : new NavX(SerialPort.Port.kMXP);
-        }
-        if (!comp)
-            return;
-        if (CLIMB_ENABLED) {
-            climbMotorFixed = new WPI_TalonFX(CLIMB_FIXED_MOTOR);
-            climbLimitSwitch = new DigitalInput(CLIMB_LIMIT_SWTICH);
-        }
-        if (INTAKE_ENABLED) {
-            intakeMotor = new WPI_TalonFX(INTAKE_MOTOR);
-            intakeMotor2 = new WPI_TalonFX(INTAKE_MOTOR_2);
-            intakeSolenoid = new DoubleSolenoid(60, PneumaticsModuleType.REVPH, INTAKE_SOLENOID_UP,
-                    INTAKE_SOLENOID_DOWN);
-            intakeProximity = new DigitalInput(INTAKE_PROXIMITY);
-        }
-        if (INDEX_ENABLED) {
-            ingestIndexMotor = new WPI_TalonFX(INDEX_INGEST_MOTOR);
-            feederIndexMotor = new WPI_TalonFX(INDEX_FEEDER_MOTOR);
-            indexProximity = new DigitalInput(INDEX_PROXIMITY);
-
-        }
-        if (SHOOTER_ENABLED) {
-            flywheelMotor1 = new WPI_TalonFX(FLYWHEEL_1);
-            flywheelMotor2 = new WPI_TalonFX(FLYWHEEL_2);
-            turretMotor = new WPI_TalonFX(TURRET);
-            hoodMotor = new CANSparkMax(HOOD, CANSparkMaxLowLevel.MotorType.kBrushless);
-        }
-        if (DRIVER_VIS_ENABLED) {
-            CameraServer.addCamera(frontCamera);
-            CameraServer.startAutomaticCapture();
-        }
-    }
+    // Simulation stuff
+    // TODO Find more accurate values
+    public static final double SIM_FULL_VELOCITY = 6000 * TalonFXConstants.RPM_TO_VELOCITY;
 }
--- src/main/java/frc/team2412/robot/Subsystems.java
+++ src/main/java/frc/team2412/robot/Subsystems.java
@@ -26,8 +26,6 @@ public class Subsystems implements Loggable {
         public static final boolean SHOOTER_TESTING = false;
     }
 
-    public final Hardware hardware;
-
     public ClimbSubsystem climbSubsystem;
 
     public DrivebaseSubsystem drivebaseSubsystem;
@@ -40,30 +38,27 @@ public class Subsystems implements Loggable {
 
     public ShooterSubsystem shooterSubsystem;
 
-    public Subsystems(Hardware h) {
+    public Subsystems() {
         boolean comp = Robot.getInstance().isCompetition();
 
-        hardware = h;
-
-        if (DRIVE_ENABLED)
-            drivebaseSubsystem = new DrivebaseSubsystem(hardware.frontLeftModule, hardware.frontRightModule,
-                    hardware.backLeftModule, hardware.backRightModule, hardware.gyro,
-                    Hardware.HardwareConstants.MODULE_MAX_VELOCITY_METERS_PER_SEC);
-        if (!comp)
+        if (DRIVE_ENABLED) {
+            drivebaseSubsystem = new DrivebaseSubsystem();
+        }
+        if (!comp) {
             return;
-        if (CLIMB_ENABLED)
-            climbSubsystem = new ClimbSubsystem(hardware.climbMotorFixed, hardware.climbLimitSwitch);
-        if (INTAKE_ENABLED)
-            intakeSubsystem = new IntakeSubsystem(hardware.intakeMotor, hardware.intakeMotor2, hardware.intakeSolenoid,
-                    hardware.intakeProximity);
-        if (SHOOTER_ENABLED) {
-            shooterSubsystem = new ShooterSubsystem(hardware.flywheelMotor1, hardware.flywheelMotor2,
-                    hardware.turretMotor, hardware.hoodMotor);
-            shooterVisionSubsystem = new ShooterVisionSubsystem();
+        }
+        if (CLIMB_ENABLED) {
+            climbSubsystem = new ClimbSubsystem();
         }
         if (INDEX_ENABLED) {
-            indexSubsystem = new IndexSubsystem(hardware.ingestIndexMotor, hardware.feederIndexMotor,
-                    hardware.indexProximity);
+            indexSubsystem = new IndexSubsystem();
+        }
+        if (INTAKE_ENABLED) {
+            intakeSubsystem = new IntakeSubsystem();
+        }
+        if (SHOOTER_ENABLED) {
+            shooterSubsystem = new ShooterSubsystem();
+            shooterVisionSubsystem = new ShooterVisionSubsystem();
         }
     }
 }

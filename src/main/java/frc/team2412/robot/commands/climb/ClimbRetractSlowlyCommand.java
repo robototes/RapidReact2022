b1dbcf4adcbb298a785dad5d635c6decbfba5a08
SumedhP <43325361+SumedhP@users.noreply.github.com>
2022-03-19 19:50:44 -0700

    Made climb slowly command require drivebase

--- src/main/java/frc/team2412/robot/commands/climb/ClimbRetractSlowlyCommand.java
+++ src/main/java/frc/team2412/robot/commands/climb/ClimbRetractSlowlyCommand.java
@@ -2,6 +2,7 @@ package frc.team2412.robot.commands.climb;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.team2412.robot.subsystem.ClimbSubsystem;
+import frc.team2412.robot.subsystem.DrivebaseSubsystem;
 import frc.team2412.robot.subsystem.IndexSubsystem;
 import frc.team2412.robot.subsystem.IntakeSubsystem;
 import frc.team2412.robot.subsystem.ShooterSubsystem;
@@ -12,9 +13,9 @@ public class ClimbRetractSlowlyCommand extends CommandBase {
 
     // needs all subsystems so doesn't run default commands in queue
     public ClimbRetractSlowlyCommand(ClimbSubsystem climbSubsystem, IntakeSubsystem intakeSubsystem,
-            IndexSubsystem indexSubsystem, ShooterSubsystem shooterSubsystem) {
+            IndexSubsystem indexSubsystem, ShooterSubsystem shooterSubsystem, DrivebaseSubsystem drivebaseSubsystem) {
         this.climbSubsystem = climbSubsystem;
-        addRequirements(climbSubsystem, intakeSubsystem, indexSubsystem, shooterSubsystem);
+        addRequirements(climbSubsystem, intakeSubsystem, indexSubsystem, shooterSubsystem, drivebaseSubsystem);
     }
 
     @Override

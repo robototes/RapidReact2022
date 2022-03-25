7fc7224157b7068700d5c3b801081eb8c2c484c7
Lucy <65467897+stewluj@users.noreply.github.com>
2022-03-24 20:32:15 -0700

    make alex happy

--- src/main/java/frc/team2412/robot/commands/autonomous/AutonomousCommand.java
+++ src/main/java/frc/team2412/robot/commands/autonomous/AutonomousCommand.java
@@ -29,8 +29,8 @@ public class AutonomousCommand extends SequentialCommandGroup {
         public static final TrapezoidProfile.Constraints K_THETA_CONTROLLER_CONSTRAINTS = new TrapezoidProfile.Constraints(
                 MAX_ANGULAR_SPEED_RADIANS_PER_SECOND, MAX_ANGULAR_SPEED_RADIANS_PER_SECOND_SQUARED);
 
-        public static final double MAX_SPEED_METERS_PER_SECOND = 1;
-        public static final double MAX_ACCELERATION_METERS_PER_SECOND_SQUARED = 1;
+        public static final double MAX_SPEED_METERS_PER_SECOND = 2;
+        public static final double MAX_ACCELERATION_METERS_PER_SECOND_SQUARED = 2;
         public static final double TRACK_WIDTH = 1.0;
         // Distance between centers of right and left wheels on robot
         public static final double WHEEL_BASE = 1.0;
@@ -47,22 +47,37 @@ public class AutonomousCommand extends SequentialCommandGroup {
     public AutonomousCommand(DrivebaseSubsystem d) {
         drivebaseSubsystem = d;
 
-        // Create config for trajectory
-        TrajectoryConfig config = new TrajectoryConfig(
+        // Create normalSpeedConfig for trajectory
+        TrajectoryConfig normalSpeedConfig = new TrajectoryConfig(
                 AutonomousCommand.AutoConstants.MAX_SPEED_METERS_PER_SECOND,
                 AutonomousCommand.AutoConstants.MAX_ACCELERATION_METERS_PER_SECOND_SQUARED)
                 // Add kinematics to ensure max speed is actually obeyed
                 .setKinematics(AutonomousCommand.AutoConstants.driveKinematics);
+        TrajectoryConfig fastSpeedConfig = new TrajectoryConfig(
+                AutonomousCommand.AutoConstants.MAX_SPEED_METERS_PER_SECOND *2,
+                AutonomousCommand.AutoConstants.MAX_ACCELERATION_METERS_PER_SECOND_SQUARED*2)
+                // Add kinematics to ensure max speed is actually obeyed
+                .setKinematics(AutonomousCommand.AutoConstants.driveKinematics);
         Trajectory trajectoryOne = TrajectoryGenerator.generateTrajectory(
-                new Pose2d(new Translation2d(7.5, 1.9), Rotation2d.fromDegrees(0)),
-                List.of(new Translation2d(7.3, 1.1)),
-                new Pose2d(new Translation2d(5.1, 1.8), Rotation2d.fromDegrees(0)),
-                config);
+                new Pose2d(7.5, 1.9, Rotation2d.fromDegrees(0)),
+                List.of(),
+                new Pose2d(7.3, 1.1, Rotation2d.fromDegrees(0)),
+                fastSpeedConfig);
         Trajectory trajectoryTwo = TrajectoryGenerator.generateTrajectory(
-                new Pose2d(new Translation2d(5.1, 1.8), Rotation2d.fromDegrees(0)),
-                List.of(new Translation2d(2.1,1.3)),
-                new Pose2d(new Translation2d(5, 2.7), Rotation2d.fromDegrees(0)),config
+                new Pose2d(7.3, 1.1, Rotation2d.fromDegrees(0)),
+                List.of(),
+                new Pose2d(5.1, 1.8, Rotation2d.fromDegrees(0)),normalSpeedConfig
         );
+        Trajectory trajectoryThree = TrajectoryGenerator.generateTrajectory(
+                new Pose2d(5.1, 1.8, Rotation2d.fromDegrees(0)),
+                List.of(),
+                new Pose2d(2.1, 1.3, Rotation2d.fromDegrees(0)),normalSpeedConfig
+        );
+        Trajectory trajectoryFour = TrajectoryGenerator.generateTrajectory(
+                new Pose2d(2.1, 1.3, Rotation2d.fromDegrees(0)),
+                List.of(),
+                new Pose2d(5, 2.7, Rotation2d.fromDegrees(0)),normalSpeedConfig
+        ); ;
 
         ProfiledPIDController thetaController = new ProfiledPIDController(
                 0.000000005, 0, 0, AutoConstants.K_THETA_CONTROLLER_CONSTRAINTS);
@@ -93,6 +108,28 @@ public class AutonomousCommand extends SequentialCommandGroup {
                 thetaController,
                 drivebaseSubsystem::updateModules,
                 drivebaseSubsystem);
+        SwerveControllerCommand swerveControllerCommandThree = new SwerveControllerCommand(
+                trajectoryThree,
+                drivebaseSubsystem::getPoseAsPoseMeters, // Functional interface to feed supplier
+                AutoConstants.driveKinematics,
+
+                // Position controllers
+                new PIDController(AutoConstants.PX_CONTROLLER, 0, 0),
+                new PIDController(AutoConstants.PY_CONTROLLER, 0, 0),
+                thetaController,
+                drivebaseSubsystem::updateModules,
+                drivebaseSubsystem);
+        SwerveControllerCommand swerveControllerCommandFour = new SwerveControllerCommand(
+                trajectoryFour,
+                drivebaseSubsystem::getPoseAsPoseMeters, // Functional interface to feed supplier
+                AutoConstants.driveKinematics,
+
+                // Position controllers
+                new PIDController(AutoConstants.PX_CONTROLLER, 0, 0),
+                new PIDController(AutoConstants.PY_CONTROLLER, 0, 0),
+                thetaController,
+                drivebaseSubsystem::updateModules,
+                drivebaseSubsystem);
 
         addCommands(
 //                new ParallelCommandGroup(
@@ -100,7 +137,7 @@ public class AutonomousCommand extends SequentialCommandGroup {
 //                        new WaitCommand(1)),
 //                new ParallelDeadlineGroup(new WaitCommand(1), new IndexShootCommand(indexSubsystem)),
 //                new IntakeSetExtendCommand(intakeSubsystem),
-                swerveControllerCommandOne, swerveControllerCommandTwo,
+                swerveControllerCommandOne, swerveControllerCommandTwo, swerveControllerCommandThree, swerveControllerCommandFour,
                 new SequentialCommandGroup());
 
 

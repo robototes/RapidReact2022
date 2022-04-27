972b58a4d2c1b0dd0be5cfd273b08ebdaba9afaf
SumedhP <43325361+SumedhP@users.noreply.github.com>
2022-04-26 20:18:51 -0700

    Remy made code spotless

--- src/main/java/frc/team2412/robot/subsystem/WpilibDrivebaseSubsystem.java
+++ src/main/java/frc/team2412/robot/subsystem/WpilibDrivebaseSubsystem.java
@@ -2,11 +2,10 @@ package frc.team2412.robot.subsystem;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class WpilibDrivebaseSubsystem extends SubsystemBase{
-    
-    public WpilibDrivebaseSubsystem(){
-        
-    }
+public class WpilibDrivebaseSubsystem extends SubsystemBase {
 
+    public WpilibDrivebaseSubsystem() {
+
+    }
 
 }

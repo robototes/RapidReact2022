780a4efa8c6f1a983bdbb8a4f9773a145d1707a8
SumedhP <43325361+SumedhP@users.noreply.github.com>
2022-04-26 19:25:23 -0700

    Remy started adding in swerve code for wpilib

---
+++ src/main/java/frc/team2412/robot/subsystem/WpilibDrivebaseSubsystem.java
@@ -0,0 +1,12 @@
+package frc.team2412.robot.subsystem;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class WpilibDrivebaseSubsystem extends SubsystemBase{
+    
+    public WpilibDrivebaseSubsystem(){
+        
+    }
+
+
+}
---
+++ src/main/java/frc/team2412/robot/util/SwerveModule.java
@@ -0,0 +1,14 @@
+package frc.team2412.robot.util;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+
+public class SwerveModule {
+
+    WPI_TalonFX driveMotor;
+    WPI_TalonFX turnMotor;
+
+    public SwerveModule(int driveMotorPort, int turnMotorPort, int turnEncoderPort){
+
+    }
+   
+}

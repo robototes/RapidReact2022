f407df098c803af24bfc2e4cbc2c8bd0413f0cb8
SumedhP <43325361+SumedhP@users.noreply.github.com>
2022-05-01 15:37:53 -0700

    Remy is coding

---
+++ src/main/java/frc/team2412/robot/util/AbsoluteSwerveModule.java
@@ -0,0 +1,73 @@
+package frc.team2412.robot.util;
+
+import com.ctre.phoenix.ErrorCode;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import com.ctre.phoenix.sensors.CANCoder;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+
+public class AbsoluteSwerveModule {
+
+    public static class SwerveModuleConstants {
+
+    }
+
+    WPI_TalonFX driveMotor;
+    WPI_TalonFX turnMotor;
+    CANCoder absoluteEncoder;
+
+    public AbsoluteSwerveModule(int driveMotorPort, int turnMotorPort, int turnEncoderPort, double turnOffset,
+            String canbus) {
+        driveMotor = new WPI_TalonFX(driveMotorPort, canbus);
+        turnMotor = new WPI_TalonFX(turnMotorPort, canbus);
+
+        if (turnEncoderPort != -1) {
+            configAbsoluteEncoder(turnEncoderPort, turnOffset, canbus);
+        }
+
+    }
+
+    private void configAbsoluteEncoder(int turnEncoderPort, double turnOffset, String canbus) {
+
+        absoluteEncoder = new CANCoder(turnEncoderPort, canbus);
+        ErrorCode encoderCreatedCheck = absoluteEncoder.configMagnetOffset(turnOffset, 100);
+
+        // I feel like this will break but 🤷‍♂️
+        if (encoderCreatedCheck.value != 0) {
+            configAbsoluteEncoder(turnEncoderPort, turnOffset, canbus);
+        }
+
+        this.turnMotor.setSelectedSensorPosition(absoluteEncoder.getAbsolutePosition());
+    }
+
+    public SwerveModuleState getState() {
+        return new SwerveModuleState(getDriveVelocityMeter(), getAngle());
+    }
+
+    public double getDriveVelocityMeter() {
+        return driveMotor.getSelectedSensorVelocity();
+    }
+
+    public Rotation2d getAngle() {
+        double degree = absoluteEncoder != null ? absoluteEncoder.getAbsolutePosition()
+                : turnMotor.getSelectedSensorPosition();
+        return new Rotation2d(Math.toRadians(degree));
+    }
+
+    public void setState(SwerveModuleState state) {
+        state = SwerveModuleState.optimize(state, getAngle());
+        driveMotor.set(state.speedMetersPerSecond);
+        turnMotor.set(ControlMode.Position, state.angle.getDegrees());
+    }
+
+    public void resetEncoder() {
+        if (absoluteEncoder != null) {
+            absoluteEncoder.setPosition(0);
+        }
+        turnMotor.setSelectedSensorPosition(0);
+        driveMotor.setSelectedSensorPosition(0);
+    }
+
+}
--- src/main/java/frc/team2412/robot/util/SwerveModule.java
+++ src/main/java/frc/team2412/robot/util/SwerveModule.java
@@ -1,49 +1,44 @@
 package frc.team2412.robot.util;
 
-import com.ctre.phoenix.ErrorCode;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
-import com.ctre.phoenix.sensors.CANCoder;
-import com.swervedrivespecialties.swervelib.ctre.CtreUtils;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 
 public class SwerveModule {
 
-    public static class SwerveModuleConstants {
+    private static final int ENCODERS_TICKS_PER_REVOLUTION = 2048;
+    private static final double WHEEL_DIAMTER_METER = 0.10033;
 
-    }
+    private static final double DRIVE_MOTOR_GEARING = 1 / 6.75;
+    private static final boolean DRIVE_MOTOR_INVERTED = true;
+    private static final double TURN_MOTOR_GEARING = 1 / 12.8;
+    private static final boolean TURN_MOTOR_INVERTED = true;
 
     WPI_TalonFX driveMotor;
     WPI_TalonFX turnMotor;
-    CANCoder absoluteEncoder;
 
-    public SwerveModule(int driveMotorPort, int turnMotorPort, int turnEncoderPort, double turnOffset, String canbus) {
+    public SwerveModule(int driveMotorPort, int turnMotorPort, String canbus) {
         driveMotor = new WPI_TalonFX(driveMotorPort, canbus);
         turnMotor = new WPI_TalonFX(turnMotorPort, canbus);
-
-        if (turnEncoderPort != -1) {
-            configAbsoluteEncoder(turnEncoderPort, turnOffset, canbus);
-        }
-
     }
 
-    private void configAbsoluteEncoder(int turnEncoderPort, double turnOffset, String canbus){
+    private void configMotors(){
+        driveMotor.setInverted(DRIVE_MOTOR_INVERTED);
+        driveMotor.configVoltageCompSaturation(12.8);
+        driveMotor.enableVoltageCompensation(true);
 
-        absoluteEncoder = new CANCoder(turnEncoderPort, canbus);
-        ErrorCode encoderCreatedCheck = absoluteEncoder.configMagnetOffset(turnOffset, 100);
-
-        // I feel like this will break but 🤷‍♂️
-        if(encoderCreatedCheck.value != 0){
-            configAbsoluteEncoder(turnEncoderPort, turnOffset, canbus);
-        }
-
-
-        this.turnMotor.setSelectedSensorPosition(absoluteEncoder.getAbsolutePosition());
+        turnMotor.setInverted(TURN_MOTOR_INVERTED);
+        turnMotor.configVoltageCompSaturation(12.8);
+        turnMotor.enableVoltageCompensation(true);
     }
 
-    
+    public void setState(SwerveModuleState state) {
+        state = SwerveModuleState.optimize(state, getAngle());
+        driveMotor.set(state.speedMetersPerSecond);
+        turnMotor.set(ControlMode.Position, state.angle.getDegrees());
+    }
 
     public SwerveModuleState getState() {
         return new SwerveModuleState(getDriveVelocityMeter(), getAngle());
@@ -54,21 +49,10 @@ public class SwerveModule {
     }
 
     public Rotation2d getAngle() {
-        double degree = absoluteEncoder != null ? absoluteEncoder.getAbsolutePosition()
-                : turnMotor.getSelectedSensorPosition();
-        return new Rotation2d(Math.toRadians(degree));
-    }
-
-    public void setState(SwerveModuleState state) {
-        state = SwerveModuleState.optimize(state, getAngle());
-        driveMotor.set(state.speedMetersPerSecond);
-        turnMotor.set(ControlMode.Position, state.angle.getDegrees());
+        return new Rotation2d(Math.toRadians(turnMotor.getSelectedSensorPosition()));
     }
 
     public void resetEncoder() {
-        if (absoluteEncoder != null) {
-            absoluteEncoder.setPosition(0);
-        }
         turnMotor.setSelectedSensorPosition(0);
         driveMotor.setSelectedSensorPosition(0);
     }

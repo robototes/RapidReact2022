7be4e6a44fbeae0c4601ab0d1f49ef283ccd7817
SumedhP <43325361+SumedhP@users.noreply.github.com>
2022-05-04 16:27:32 -0700

    Remy forgor about radians

--- src/main/java/frc/team2412/robot/commands/shooter/ShootingWhileMovingV2Command.java
+++ src/main/java/frc/team2412/robot/commands/shooter/ShootingWhileMovingV2Command.java
@@ -57,7 +57,7 @@ public class ShootingWhileMovingV2Command extends CommandBase {
         shooter.setFlywheelRPM(rpmHoodValues.getRPM());
         shooter.setHoodAngle(rpmHoodValues.getAngle());
 
-        double turretDegreeChange = Math.atan(theoreticalTargetPosition.getY() / theoreticalTargetPosition.getX());
+        double turretDegreeChange = Math.atan(theoreticalTargetPosition.getY() / theoreticalTargetPosition.getX()) * 180;
         shooter.updateTurretAngle(turretDegreeChange);
         // Don't do wrap around yet since we don't have 360,
     }
